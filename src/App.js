b161a1d8b8c949a31324feff4791f91bf3c3f9e4
aditraglia <aditraglia@gmail.com>
2021-12-29 14:34:45 +0100

    todo list first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Form from './components/Form';
+import List from "./components/List";
+import './style/App.css';
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todoList, setTodoList] = useState([]);
+
+  //Use effect con un array vuoto come secondo parametro
+  //viene esguito solo una volta, ovvero all'avvio dell'applicazione
+  useEffect( () => {
+    getLocalItems();
+  }, []);
+
+  //Le funzioni dentro use effect vengono eseguite ogni volta che cambia todoList
+  useEffect( () => {
+    saveToLocal();
+  }, [todoList]);
+
+  function saveToLocal() {
+    localStorage.setItem("todoList", JSON.stringify(todoList));
+  }
+
+  function getLocalItems() {
+    if (localStorage.getItem("todoList") === null) {
+      localStorage.setItem("todoList", JSON.stringify([]));
+    } else {
+      let localItems = JSON.parse(localStorage.getItem("todoList"));
+      setTodoList(localItems);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Todo List</h1>
       </header>
+      <Form 
+        inputText={inputText} 
+        setInputText={setInputText}
+        todoList={todoList}
+        setTodoList={setTodoList}
+      />
+      <List 
+        todoList={todoList}
+        setTodoList={setTodoList}
+      />
     </div>
   );
 }
